02946c11e4e2ba214ea7e2f113d653cbb0edcf51
IOAyman <theycallmethedr@gmail.com>
2015-12-28 00:15:38 +0100

    fixes

--- yacine.ps1
+++ yacine.ps1
@@ -1,3 +1,5 @@
+#Rquires -RunAsAdministrator
+
 $ydl = "$Env:windir\system32\youtube-dl.exe"
 $ydlurl = "https://yt-dl.org/downloads/latest/youtube-dl.exe"
 
@@ -5,16 +7,22 @@ $ydlurl = "https://yt-dl.org/downloads/latest/youtube-dl.exe"
 if (-Not(test-path $ydl)){
   write-host -foreground red "youtube-dl not found!"
   write-host -foreground red "Attempting to download ..."
-  if ( -Not (
-    (
-      [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
-    ).IsInRole( [Security.Principal.WindowsBuiltInRole] "Administrator" )
-  )) {
-      write-host -background yellow -foreground red "You must be Administrator to install youtube-dl."
-      exit-pssession
-    }
-  $wc = New-Object system.net.webclient
-  $wc.downloadfile($ydlurl, $ydl
+  #if ( -Not (
+  #  (
+  #    [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+  #  ).IsInRole( [Security.Principal.WindowsBuiltInRole] "Administrator" )
+  #)) {
+  #    write-host -background yellow -foreground red "You must be Administrator to install youtube-dl."
+  #    exit-pssession
+  #  }
+  try {
+    $wc = New-Object system.net.webclient
+    $wc.downloadfile($ydlurl, $ydl)
+  } catch {
+    write-host -background yellow -foreground red "You must be Administrator to install youtube-dl."
+    read-host
+    exit-pssession
+  }
 }
 
 
@@ -35,4 +43,6 @@ if ($audio -eq 'y' -or $audio -eq 'Y') {
 } else {
   youtube-dl $url
 }
+
+write-host -foreground green "DONE!"
 read-host
